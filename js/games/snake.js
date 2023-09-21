30c35df21e0d4d53862d4bcb8e64c0836b435ea0
Pedro Gardim <me@pedrogardim.com>
2023-09-21 11:03:54 +0200

    Basic draw tiles

--- js/games/snake.js
+++ js/games/snake.js
@@ -3,21 +3,50 @@ class SnakeGameController {
     this.init();
   }
   init() {
-    console.log("hey, inited");
-    this.canvas = document.getElementById("game");
-    this.ctx = this.canvas.getContext("2d");
+    this.initCanvas();
     this.gameSize = [32, 20];
-    this.snakeVector = [2, 1];
-    setInterval(this.update, 1000 / 2);
+    this.snakeVector = [1, 0];
+    this.snakeTiles = [[16, 10]];
+    setInterval(() => this.update(), 1000 / 2);
     document.addEventListener("keydown", this.onKeyPress);
   }
-  onKeyPress(event) {
-    console.log(event);
-  }
   update() {
-    console.log("updated");
+    let lastTile = this.snakeTiles[this.snakeTiles.length - 1];
+    let newTile = lastTile.map((d, i) => d + this.snakeVector[i]);
+    this.snakeTiles.push(newTile);
+    this.draw();
   }
-  draw() {}
+  initCanvas() {
+    this.canvas = document.getElementById("game");
+    this.ctx = this.canvas.getContext("2d");
+  }
+  draw() {
+    this.ctx.fillRect;
+    this.snakeTiles.forEach((tile) => {
+      this.ctx.fillStyle = "#FF0000";
+      this.ctx.fillRect(
+        (this.getTileSize()[0] + 2) * tile[0],
+        (this.getTileSize()[1] + 2) * tile[1],
+        this.getTileSize()[0],
+        this.getTileSize()[1]
+      );
+    });
+  }
+  // utils
+  getRandomPosition() {
+    return this.gameSize.map((dim) => Math.floor(Math.random() * dim));
+  }
+  getTileSize() {
+    return [
+      this.canvas.width / this.gameSize[0],
+      this.canvas.height / this.gameSize[1],
+    ];
+  }
 }
 
 const dinoGame = new SnakeGameController();
+
+// let num = parseFloat(prompt("Insertar número"));
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${num} x ${i} = ${num * i}`);
+// }
